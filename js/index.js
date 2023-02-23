6bab3afcbf7bf2ea0d56831e7843e33b50619eb6
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-02-23 15:48:50 +0200

    add-js-markup

--- js/index.js
+++ js/index.js
@@ -7,6 +7,38 @@ const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const action = ['-', '+', 'x', '/', '%', '+/-'];
 
 //screen
+
+// const container = document.querySelector('.calc');
+
+function createMarkup() {
+  return `<div class="calc__container">
+        <p class="calc__total">0</p>
+        <div class="calc__screen"></div>
+        <div class="btn__container">
+          <button class="calc__btn calc__btn--clear ac">AC</button>
+          <button class="calc__btn calc__btn--clear plusminus">+/-</button>
+          <button class="calc__btn calc__btn--clear interest">%</button>
+          <button class="calc__btn calc__btn--operator division">/</button>
+          <button class="calc__btn one">1</button>
+          <button class="calc__btn two">2</button>
+          <button class="calc__btn three">3</button>
+          <button class="calc__btn calc__btn--operator multiply">x</button>
+          <button class="calc__btn four">4</button>
+          <button class="calc__btn five">5</button>
+          <button class="calc__btn six">6</button>
+          <button class="calc__btn calc__btn--operator minus">-</button>
+          <button class="calc__btn seven">7</button>
+          <button class="calc__btn eight">8</button>
+          <button class="calc__btn nine">9</button>
+          <button class="calc__btn calc__btn--operator plus">+</button>
+          <button class="calc__btn calc__btn--zero zero">0</button>
+          <button class="calc__btn point">.</button>
+          <button class="calc__btn calc__btn--operator amount">=</button>
+        </div>
+      </div>`;
+}
+document.body.insertAdjacentHTML('afterbegin', createMarkup());
+
 const out = document.querySelector('.calc__total');
 const clear = document.querySelector('.ac');
 const btnS = document.querySelector('.btn__container');
@@ -20,6 +52,7 @@ function clearAll() {
   sign = '';
   finish = false;
   out.textContent = 0;
+  return;
 }
 
 function onClickBtn(e) {
